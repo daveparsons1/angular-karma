79bc331b64e6cbebbbc6dbd457fa897420715996
Dave Parsons <d.parsons1@hotmail.com>
2016-05-27 13:43:26 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+angular.module('myApp', [])
+  .factory('Person', function () {
+    return function Person (name) {
+      this.name = name;
+    };
+  });
---
+++ tests/app.spec.js
@@ -0,0 +1,18 @@
+describe('Person', function () {
+
+  var Person;
+  beforeEach(module('myApp'));
+  beforeEach(inject(function (_Person_) {
+    Person = _Person_;
+  }));
+
+  describe('Constructor', function () {
+
+    it('assigns a name', function () {
+      //expect(new Person('Ben')).to.have.property('name', 'Ben');
+      expect(new Person('Ben')).toBeDefined();
+    });
+
+  });
+
+});
